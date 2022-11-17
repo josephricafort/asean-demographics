c7eb6a8b9ce3cbbaaba4208f3099ac0f85c42b67
Joseph Ricafort <josephricafortjr@gmail.com>
2022-11-17 10:38:48 +0800

    Fetch and wrangle all needed data

--- r/scripts/explore.R
+++ r/scripts/explore.R
@@ -2,6 +2,7 @@ library(tidyverse)
 library(jsonlite)
 library(ggmap)
 library(googlesheets4)
+library(lingtypology)
 
 sea_countries <- c("BX", "CB", "ID", "LA", "MY", "BM", "RP", "SN", "TH", "VM")
 sea_countries_full <- c("Brunei", "Cambodia", "Indonesia", "Laos",
@@ -19,8 +20,10 @@ GOOGLE_MAP_API_KEY <- "AIzaSyDLuH3fAvlTJg-XPX_Ng_-4acr6FEShLfw"
 COLDAT_COLONIES_URL <- "https://docs.google.com/spreadsheets/d/1Qu8UmA-wJU9vohV6wmvDk0RcS7_4MvIz8OviELQ1dkw/edit#gid=1614706879"
 COLDAT_DYADS_URL <- "https://docs.google.com/spreadsheets/d/1uXKX4b8CySp8E_W2B8KeBJknTeYppDjljv6VF2qLc4A/edit#gid=1350853870"
 
-#--- COLONIAL DATA ---#
 
+#--- FETCH DATASETS --- #
+
+# Colonial data
 # Read colonial dataset from Google sheets
 gs4_deauth()
 raw_coldat_data <- read_sheet(COLDAT_DYADS_URL)
@@ -33,9 +36,7 @@ desired_coldat_data <- raw_coldat_data %>%
   # pivot_longer(colstart_max:colend_mean, names_to = "col_marker",
   #              values_to = "year")
 
-
-#--- DEMOGRAPHICS DATA ---#
-
+# Demographics Data
 raw_data <- tibble()
 # Loop through fetch data for every country
 for (c in sea_countries){
@@ -47,11 +48,31 @@ for (c in sea_countries){
 }
 
 desired_data <- raw_data %>%
-  top_n(500, PopulationPercentUN) # %>%
-  # left_join(desired_coldat_data, by = c("ROG3" = "country_code"))
+  top_n(500, PopulationPercentUN) %>%
+  left_join(desired_coldat_data, by = c("ROG3" = "country_code")) %>%
+  select(ROG3, Ctry, PeopleCluster, AffinityBloc,
+         PrimaryReligion, colonizer,
+         Population, Longitude, Latitude,
+         ROL3, PrimaryLanguageName)
 
 toJSON(desired_data) %>%
   write("data/output/aseanEthnicGroup.json")
+
+# Language Families
+glotto_data <- as_tibble(glottolog) %>%
+  filter(iso %in% unique(desired_data$ROL3)) %>%
+  select(iso, level, area, affiliation) %>%
+  rename(lang_iso = iso,
+         lang_level = level,
+         lang_area = area,
+         lang_affiliation = affiliation) %>%
+  mutate(lang_family = str_extract(lang_affiliation, '[\\w\\s\\-]*'))
+
+combined_data <- desired_data %>%
+  left_join(glotto_data, by = c("ROL3" = "lang_iso"))
+
+
+#--- VISUALIZE --- #
 
 # Visualize the distribution across Southeast Asia
 register_google(GOOGLE_MAP_API_KEY)
@@ -64,13 +85,13 @@ asean_map <- get_map(location,
                      color = "bw")
 
 ggmap(asean_map, darken = c(0.5, "white")) +
-  geom_point(data = desired_data,
+  geom_point(data = combined_data,
              aes(Longitude, Latitude,
                  size = Population,
-                 color = PrimaryReligion # PeopleCluster # AffinityBloc,
+                 color = PeopleCluster # PrimaryReligion # PeopleCluster # AffinityBloc,
                  ),
-             fill = "transparent") # +
-  # theme(legend.position="none")
+             fill = "transparent")
+  + theme(legend.position="none")
 
 # Visualize the timeline of colonization per country
 coldat_malaysia_tbl <- desired_coldat_data %>%
@@ -94,3 +115,13 @@ ggplot(toplot_coldat_data) +
     alpha = 0.95) +
   coord_flip() +
   labs(x = "Country", y = "Year")
+
+# Facet visualize
+ggplot(combined_data, aes(
+    x = lang_family, 
+    y = Population,
+    fill = lang_family)) +
+  geom_col() +
+  facet_grid(cols = vars(Ctry)) +
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) +
+  coord_flip()
