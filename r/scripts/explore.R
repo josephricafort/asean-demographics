69c3addea955ca26173b0522347a2dd1ab7b3c95
Joseph Ricafort <josephricafortjr@gmail.com>
2022-11-22 19:05:52 +0800

    Refine distribution data, include all population

--- r/scripts/explore.R
+++ r/scripts/explore.R
@@ -48,7 +48,7 @@ for (c in sea_countries){
 }
 
 desired_data <- raw_data %>%
-  top_n(500, PopulationPercentUN) %>%
+  # top_n(500, PopulationPercentUN) %>%
   left_join(desired_coldat_data, by = c("ROG3" = "country_code")) %>%
   select(ROG3, Ctry, PeopleCluster, AffinityBloc,
          PrimaryReligion, colonizer,
@@ -96,8 +96,7 @@ ggmap(asean_map, darken = c(0.5, "white")) +
                  color = lang_family, # PrimaryReligion # PeopleCluster # AffinityBloc,
                  shape= PrimaryReligion
                  ),
-             fill = "transparent")
-  # + theme(legend.position="none")
+             fill = "transparent") #+ theme(legend.position="none")
 
 # Visualize the timeline of colonization per country
 coldat_malaysia_tbl <- desired_coldat_data %>%
@@ -148,34 +147,96 @@ ggplot(combined_data, aes(
 
 
 #--- REPRESENT COMMUNITIES ---#
+OTHER_CLUSTERS = "Other Clusters"
+OTHER_RELIGIONS = "Other Religions"
+OTHER_LANGFAMS = "Other Language Families"
 
-distrib_data <- combined_data %>%
+strat_data <- combined_data %>%
+  filter(!is.na(Population)) %>%
   rename(langFamily = lang_family) %>%
-  select(Ctry, PrimaryReligion, langFamily, Population) %>%
-  group_by(Ctry, PrimaryReligion, langFamily) %>%
+  select(Ctry, PrimaryReligion, langFamily, PeopleCluster, Population) %>%
+  group_by(Ctry, PrimaryReligion, langFamily, PeopleCluster) %>%
   summarize(Population = sum(Population)) %>%
   ungroup() %>%
   mutate(percent = (Population / sum(Population) * 100),
-         pop_thousand = round(percent * 100)) %>%
-  arrange(desc(percent)) %>%
-  filter(pop_thousand != 0)
+         popThou = round(percent * 10),
+         popTenThou = round(percent * 100))
 
+majority_data <- strat_data %>%
+  filter(popThou != 0)
+
+total_maj_data <- majority_data %>%
+  summarize(Population = sum(Population))
+
+strat_min_data <- strat_data %>%
+  filter(popThou == 0) %>%
+  bind_rows(tibble(
+    total_maj_data,
+    PeopleCluster = OTHER_CLUSTERS
+    )) %>%
+  arrange(Ctry) %>%
+  group_by(Ctry, langFamily) %>%
+  summarize(Population = sum(Population)) %>%
+  ungroup() %>%
+  mutate(percent = (Population / sum(Population) * 100),
+         popThou = round(percent * 10),
+         popTenThou = round(percent * 100))
+
+minority_data <- strat_min_data %>% 
+  mutate(PeopleCluster = OTHER_CLUSTERS,
+         PrimaryReligion = OTHER_RELIGIONS) %>%
+  filter(popThou > 0 & !is.na(Ctry))
+
+superminority_data <- strat_min_data %>%
+  filter(popThou == 0) %>%
+  anti_join(minority_data, by = "Ctry", "langFamily") %>%
+  group_by(Ctry) %>%
+  summarize(Population = sum(Population)) %>%
+  ungroup() %>%
+  bind_rows(total_maj_data) %>%
+  mutate(percent = (Population / sum(Population) * 100),
+         popThou = round(percent * 10),
+         popTenThou = round(percent * 100)) %>%
+  mutate(PeopleCluster = OTHER_CLUSTERS,
+         PrimaryReligion = OTHER_RELIGIONS,
+         langFamily = OTHER_LANGFAMS)
+
+# Brunei special case - since all of Brunei's population
+# is represented by a single unit, we need to recode
+# the wordings of 'Other' to 'All' (Ex.: Other Clusters
+# will become All Clusters)
+brunei_data <- superminority_data %>%
+  filter(Ctry == "Brunei") %>%
+  mutate(across(PeopleCluster:langFamily, 
+                function (c){ 
+                  return (str_replace(c, "Other", "All")) 
+                }))
+  
+distrib_data <- majority_data %>%
+  bind_rows(minority_data, superminority_data) %>%
+  ungroup() %>%
+  filter(!is.na(Ctry)) %>%
+  filter(Ctry != "Brunei") %>% # Replace with the tweaked one above
+  bind_rows(brunei_data) %>%
+  arrange(Ctry, PrimaryReligion, langFamily)
+
+print(distrib_data, n = nrow(distrib_data))
 
 # Quickly visualize
 ggplot(distrib_data) +
   geom_col(aes(x = langFamily,
-               y = pop_thousand,
+               y = popThou,
                fill = PrimaryReligion)) + 
   facet_wrap(vars(Ctry), scales = "free_y") +
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1)) +
   coord_flip()
 
 ggplot(distrib_data %>%
-         mutate(pop_thou_log = log10(pop_thousand))) +
+         mutate(popThouLog = log10(popThou))) +
   geom_tile(aes(x = langFamily,
                y = PrimaryReligion,
-               fill = pop_thou_log)) +
-  facet_wrap(vars(Ctry), scales = "free") +
+               fill = popThouLog)) +
+  facet_wrap(vars(Ctry), scales = "free_x") +
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1)) +
   # scale_fill_continuous(palette = "RdYlBu")
   # scale_fill_gradient2(
@@ -184,7 +245,9 @@ ggplot(distrib_data %>%
   #   high = "brown", 
   #   midpoint = 1
   # )
-  scale_fill_viridis_c(option = "magma", direction = -1)
+  scale_fill_viridis_c(option = "magma",
+                       direction = -1,
+                       na.value = "white")
 
 toJSON(distrib_data) %>%
   write("data/output/aseanDistributionData.json")
